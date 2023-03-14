a8a68cc57f1f613d5316744f9b7739a5e88bc81b
Luiz Augusto <augustomoraes90@gmail.com>
2023-03-14 00:26:59 -0300

    improv: add component to list, delete, update and filter notes (on angular)

---
+++ note-web/src/app/components/notes/list/note-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { NoteService } from '../note.service';
+
+@Component({
+  selector: 'app-note-list',
+  templateUrl: './note-list.component.html',
+  styleUrls: []
+})
+export class NoteListComponent implements OnInit {
+
+  public notes: any[] = [];
+  public searchTerm: string = '';
+
+  constructor(private noteService: NoteService) { }
+
+  ngOnInit() {
+    this.reload();
+  }
+
+  async reload() {
+    const notes = await this.noteService.getNotes();
+    this.notes = notes;
+  }
+
+
+  delete(id: string) {
+    this.noteService.deleteNote(id);
+    this.reload();
+  }
+
+}
