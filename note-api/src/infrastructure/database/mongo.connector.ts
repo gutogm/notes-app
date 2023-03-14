5e887003cffe0835a067d0a4166a0c2f05457a74
Luiz Augusto <augustomoraes90@gmail.com>
2023-03-14 01:33:13 -0300

    improv: add env var to define mongo access

--- note-api/src/infrastructure/database/mongo.connector.ts
+++ note-api/src/infrastructure/database/mongo.connector.ts
@@ -2,7 +2,7 @@ import mongoose, { ConnectOptions } from "mongoose";
 
 export class MongoConnector {
   public async connect(): Promise<void> {
-    const url = 'mongodb://127.0.0.1:27017/notes';
+    const url = process.env.MONGO_URL || `mongodb://127.0.0.1:27017/notes`;
     const options: ConnectOptions = {
     };
     await mongoose.connect(url, options)
